08d4cba6d28b0d4f6c29dee594f5f0b070eb4fb7
Florin Slevoaca <florin.slevoaca@ionos.com>
2022-03-09 23:29:55 +0200

    add shell client test

---
+++ src/test/java/io/jtest/utils/clients/shell/ShellClientTest.java
@@ -0,0 +1,22 @@
+package io.jtest.utils.clients.shell;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShellClientTest {
+
+    @Test
+    public void testSimpleShellCommand() {
+        ShellClient client = new ShellClient();
+        assertTrue(client.execute("bash", "-c", "ls -alh").contains("total"));
+    }
+
+    @Test
+    public void checkDirectory() {
+        ShellClient client = new ShellClient(new ProcessBuilder().directory(new File("src/test/resources")));
+        assertTrue(client.execute("bash", "-c", "pwd").trim().endsWith("/src/test/resources"));
+    }
+}
