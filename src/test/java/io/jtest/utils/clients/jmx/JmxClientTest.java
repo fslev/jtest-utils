e474de2fd7c4a03c0a68e0891512d300b1ac4da1
Florin <florin.slevoaca@ionos.com>
2022-06-23 13:53:17 +0300

    enhance test

--- src/test/java/io/jtest/utils/clients/jmx/JmxClientTest.java
+++ src/test/java/io/jtest/utils/clients/jmx/JmxClientTest.java
@@ -5,14 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import javax.management.MalformedObjectNameException;
 
-@Disabled
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class JmxClientTest {
 
     @Test
-    public void testJmxConnection() throws MalformedObjectNameException {
+    @Disabled
+    public void testJmxProxy() throws MalformedObjectNameException {
         JmxClient jmxClient = new JmxClient("service:jmx:rmi:///jndi/rmi://domnexfrontmwqa01.mw.server.lan:11193/server",
                 "control", "shu8cooS");
         PollerMBean proxy = jmxClient.getMBeanProxy("MigraeneDomain:name=MigraeneDomainPollerMBean", PollerMBean.class);
         proxy.switchPollerActive();
+    }
+
+    @Test
+    public void testInvalidJmxConnection() {
+        Exception e = assertThrows(RuntimeException.class, () -> new JmxClient("service:jmx:rmi:///jndi/rmi://invalid.test.de:11193/server", "control", "shu8cooS"));
+        assertTrue(e.getMessage().contains("Failed to retrieve"));
     }
 }
