dc263745f8820409e7080b6d099ae3b605604bc0
fslevoaca <florin.slevoaca@ionos.com>
2023-11-26 19:21:31 +0200

    refactor code

--- src/test/java/io/jtest/utils/PlainHttpResponseUtils.java
+++ src/test/java/io/jtest/utils/PlainHttpResponseUtils.java
@@ -3,14 +3,16 @@ package io.jtest.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.cfg.JsonNodeFeature;
 import io.jtest.utils.matcher.http.PlainHttpResponse;
 
 public class PlainHttpResponseUtils {
 
     public static PlainHttpResponse from(String content) {
-        ObjectMapper mapper = new ObjectMapper().setNodeFactory(JsonNodeFactory.withExactBigDecimals(true))
-                .enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS).configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
+        ObjectMapper mapper = new ObjectMapper()
+                .configure(JsonNodeFeature.STRIP_TRAILING_BIGDECIMAL_ZEROES, false)
+                .enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS)
+                .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
                 .configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
         try {
             return mapper.readValue(content, PlainHttpResponse.class);
